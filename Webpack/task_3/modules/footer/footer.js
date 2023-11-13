fc6a8709ed5cfca1b2f95fb3ae9dc76117daebcf
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-11-12 20:57:51 -0500

    footer.js was updated

---
+++ Webpack/task_3/modules/footer/footer.js
@@ -0,0 +1,9 @@
+// Import jQuery and the associated CSS for the footer
+import $ from 'jquery';
+import './footer.css';
+
+// Execute the following code when the DOM is ready
+$(function() {
+    // Append a paragraph with copyright text to the body
+    $('body').append('<p>Copyright - Holberton School</p>');
+});
