23301a27738f18e0abef70996beb9813109f898a
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-11-21 23:20:51 -0500

    Add 'Directors' interface extending 'Teacher', and example usage

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -8,13 +8,29 @@ interface Teacher {
     [key: string]: string | boolean | number | undefined; // To allow any attribute
   }
 
-  // Example of use
-  const teacher3: Teacher = {
-    firstName: 'Fracisco',
-    fullTimeEmployee: false,
-    lastName: 'Contreras',
-    location: 'Lima',
-    contract: false,
-  };
+// Example of use
+const teacher3: Teacher = {
+  firstName: 'Fracisco',
+  fullTimeEmployee: false,
+  lastName: 'Contreras',
+  location: 'Lima',
+  contract: false,
+};
 
-  console.log(teacher3);
+console.log(teacher3);
+
+// Definition of the 'Directors' interface that extends 'Teacher'
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+// Example of use with 'Directors' interface
+const director1: Directors = {
+  firstName: 'Carlos',
+  lastName: 'Gutierrez',
+  location: 'Lima',
+  fullTimeEmployee: true,
+  numberOfReports: 25,
+};
+
+console.log(director1);
