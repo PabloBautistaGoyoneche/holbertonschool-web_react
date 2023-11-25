6fc79b4cc719d2e1a5fc784c5a1f3e2a0f0ea404
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-11-25 08:28:20 -0500

    chore: Add configuration files and dependency management

---
+++ TypeScript/task_3/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin'); // Agrega esta línea
+
+module.exports = {
+  entry: "./js/main.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Development"
+    })
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }, // Corrige el punto y coma aquí
+  optimization: {
+    minimize: false, // Desactiva la minimización
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+        },
+        extractComments: false,
+      }),
+    ],
+  }, // Corrige los dos puntos aquí
+};
