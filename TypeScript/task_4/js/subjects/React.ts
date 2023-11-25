4b131489c2529c635965e9966bb044925c706b6f
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-11-25 11:52:07 -0500

    feat: add React class and extend Teacher interface in Subjects namespace

--- TypeScript/task_4/js/subjects/React.ts
+++ TypeScript/task_4/js/subjects/React.ts
@@ -1,21 +1,23 @@
-/// <reference path="./Teacher.ts" />
-/// <reference path="./Subject.ts" />
-
 namespace Subjects {
-  interface Teacher {
-    experienceTeachingReact?: number;
+  // Extends the Teacher interface within the Subjects namespace
+  export interface Teacher {
+    experienceTeachingReact?: number; // Optional attribute for React
   }
 
+  // Class 'React' extending from 'Subject'
   export class React extends Subject {
+    // Method returning requirements for React
     getRequirements(): string {
-      return "Here is the list of requirements for React";
+      return 'Here is the list of requirements for React';
     }
 
+    // Method to check available React teacher
     getAvailableTeacher(): string {
-      if (this._teacher && this._teacher.experienceTeachingReact) {
-        return `Available Teacher: ${this._teacher.firstName}`;
+      // Checks if the teacher has experience teaching React
+      if (this.teacher.experienceTeachingReact) {
+        return `Available Teacher: ${this.teacher.firstName}`;
       } else {
-        return "No available teacher";
+        return 'No available teacher';
       }
     }
   }
