f930314e82ef6893f04a6dd3f67a1901e3892c58
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-11-25 11:55:35 -0500

    feat: add getAvailableTeacher method to Cpp class

--- TypeScript/task_4/js/subjects/Cpp.ts
+++ TypeScript/task_4/js/subjects/Cpp.ts
@@ -1,21 +1,23 @@
-/// <reference path="./Teacher.ts" />
-/// <reference path="./Subject.ts" />
-
 namespace Subjects {
-  interface Teacher {
-    experienceTeachingC?: number;
+  // Extends the Teacher interface within the Subjects namespace
+  export interface Teacher {
+    experienceTeachingC?: number; // Optional attribute for C++
   }
 
+  // Class 'Cpp' extending from 'Subject'
   export class Cpp extends Subject {
+    // Method returning requirements for C++
     getRequirements(): string {
-      return "Here is the list of requirements for Cpp";
+      return 'Here is the list of requirements for Cpp';
     }
 
+    // Method to check available C++ teacher
     getAvailableTeacher(): string {
-      if (this._teacher && this._teacher.experienceTeachingC) {
-        return `Available Teacher: ${this._teacher.firstName}`;
+      // Checks if the teacher has experience teaching C++
+      if (this.teacher.experienceTeachingC) {
+        return `Available Teacher: ${this.teacher.firstName}`;
       } else {
-        return "No available teacher";
+        return 'No available teacher';
       }
     }
   }
