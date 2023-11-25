8ec44e9b65203b92b7c0a6e88848be64603ed6e5
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-11-25 11:48:03 -0500

    Update: define Teacher interface in Subjects namespace

--- TypeScript/task_4/js/subjects/Teacher.ts
+++ TypeScript/task_4/js/subjects/Teacher.ts
@@ -1,6 +1,8 @@
+// Defining the Teacher interface within the Subjects namespace
 namespace Subjects {
+  // Interface representing teacher details
   export interface Teacher {
-    firstName: string;
-    lastName: string;
+    firstName: string; // First name of the teacher
+    lastName: string;  // Last name of the teacher
   }
 }
