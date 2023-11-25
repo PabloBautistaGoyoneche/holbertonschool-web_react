18160b04c6cfc3d50409aab812478d35a9cbf9eb
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-11-25 17:18:37 -0500

    chore: Add constants and Teacher object, implement log logic for Cpp, Java and React

--- TypeScript/task_4/js/main.ts
+++ TypeScript/task_4/js/main.ts
@@ -19,3 +19,43 @@ CRUD.updateRow(newRowID, updatedRow);
 
 // Delete Row
 CRUD.deleteRow(newRowID);
+
+// Create and export constants for subjects
+export const cpp = 'Cpp Subjects';
+export const java = 'Java Subjects';
+export const react = 'React Subjects';
+
+// Create and export a Teacher object cTeacher with experienceTeachingC = 10
+export const cTeacher = {
+  experienceTeachingC: 10,
+};
+
+// Function to log information for a subject
+function logSubjectInfo(subject: string, teacher: any) {
+  console.log(subject);
+  console.log(`Set ${teacher} as the teacher.`);
+  console.log(`Requirements: ${getRequirements(subject)}`);
+  console.log(`Available Teacher: ${getAvailableTeacher(subject, teacher)}`);
+  console.log('-------------------------------------------');
+}
+
+// Function to get requirements for a subject
+function getRequirements(subject: string): string {
+  // Placeholder implementation, replace with actual logic
+  return `Requirements for ${subject}`;
+}
+
+// Function to get available teacher for a subject
+function getAvailableTeacher(subject: string, teacher: any): string {
+  // Placeholder implementation, replace with actual logic
+  return `Available Teacher for ${subject}: ${teacher}`;
+}
+
+// Log information for C++ subject
+logSubjectInfo(cpp, cTeacher);
+
+// Log information for Java subject
+logSubjectInfo(java, cTeacher);
+
+// Log information for React subject
+logSubjectInfo(react, cTeacher);
