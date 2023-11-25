4473f88aeebbc1d25c6e0cd6b2ba0a8547db7015
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-11-25 11:54:37 -0500

    feat: add getAvailableTeacher method to Java class

--- TypeScript/task_4/js/subjects/Java.ts
+++ TypeScript/task_4/js/subjects/Java.ts
@@ -1,23 +1,24 @@
-/// <reference path="./Teacher.ts" />
-/// <reference path="./Subject.ts" />
-
 namespace Subjects {
-    interface Teacher {
-      experienceTeachingJava?: number;
+  // Extends the Teacher interface within the Subjects namespace
+  export interface Teacher {
+    experienceTeachingJava?: number; // Optional attribute for Java
+  }
+
+  // Class 'Java' extending from 'Subject'
+  export class Java extends Subject {
+    // Method returning requirements for Java
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java';
     }
-  
-    export class Java extends Subject {
-      getRequirements(): string {
-        return "Here is the list of requirements for Java";
-      }
-  
-      getAvailableTeacher(): string {
-        if (this._teacher && this._teacher.experienceTeachingJava) {
-          return `Available Teacher: ${this._teacher.firstName}`;
-        } else {
-          return "No available teacher";
-        }
+
+    // Method to check available Java teacher
+    getAvailableTeacher(): string {
+      // Checks if the teacher has experience teaching Java
+      if (this.teacher.experienceTeachingJava) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return 'No available teacher';
       }
     }
   }
-  
+}
