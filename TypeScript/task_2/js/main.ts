87fba2cbefb783088435e182072a3698628c2bd9
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-11-24 22:48:02 -0500

    Enhanced type handling with predicates and execution for Director and Teacher tasks.

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -1,61 +1,77 @@
-// task_2/js/main.ts
-
 // DirectorInterface
 interface DirectorInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workDirectorTasks(): string;
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// TeacherInterface
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Class Director
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+// Class Teacher
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+// Function createEmployee
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+// Example of use with createEmployee function
+console.log(createEmployee(200)); // Teacher
+console.log(createEmployee(1000)); // Director
+console.log(createEmployee("$500")); // Director
+
+
+// Function isDirector
+function isDirector(employee: Director | Teacher): employee is Director {
+    return employee instanceof Director;
   }
   
-  // TeacherInterface
-  interface TeacherInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workTeacherTasks(): string;
-  }
-  
-  // Class Director
-  class Director implements DirectorInterface {
-    workFromHome(): string {
-      return "Working from home";
-    }
-  
-    getCoffeeBreak(): string {
-      return "Getting a coffee break";
-    }
-  
-    workDirectorTasks(): string {
-      return "Getting to director tasks";
-    }
-  }
-  
-  // Class Teacher
-  class Teacher implements TeacherInterface {
-    workFromHome(): string {
-      return "Cannot work from home";
-    }
-  
-    getCoffeeBreak(): string {
-      return "Cannot have a break";
-    }
-  
-    workTeacherTasks(): string {
-      return "Getting to work";
-    }
-  
-  }
-  
-  // Function createEmployee
-  function createEmployee(salary: number | string): Director | Teacher {
-    if (typeof salary === 'number' && salary < 500) {
-      return new Teacher();
+  // Function executeWork
+  function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+      return employee.workDirectorTasks();
     } else {
-      return new Director();
+      return employee.workTeacherTasks();
     }
   }
   
-  // Example of use with createEmployee function
-  console.log(createEmployee(200)); // Teacher
-  console.log(createEmployee(1000)); // Director
-  console.log(createEmployee('$500')); // Director
+  // Example of use with executeWork function
+  console.log(executeWork(createEmployee(200))); // Getting to work
+  console.log(executeWork(createEmployee(1000))); // Getting to director tasks
   
